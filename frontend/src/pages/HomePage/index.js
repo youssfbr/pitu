4063ab083ab13cea59ba6932144dd07df6a140c8
alisson youssf <youssfbr@gmail.com>
2020-11-28 20:48:50 -0300

    frontend environment, adjusts

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header';
 import { Container, InputGroup, FormControl, Button, Alert, Spinner } from 'react-bootstrap';
 import { ContentContainer, Form, AdsBlock } from './styles';
 import ShortenerService from '../../services/shortenerService';
+import vars from '../../configs/vars';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -70,14 +71,14 @@ class HomePage extends React.Component {
                   <InputGroup className="mb-3">
                     <FormControl
                       autoFocus={true}
-                      defaultValue={`https://pitu.tk/${code}`}
+                      defaultValue={ vars.HOST_APP + code } 
                       ref={(input) => this.inputURL = input }
                     />
                     <InputGroup.Append>
                       <Button variant="outline-secondary" onClick={() => this.copyToClipboard() }> Copiar </Button>
                     </InputGroup.Append>
                   </InputGroup>
-                  <p>Para acompanhar as estatísticas, acesse http://pitu.tk/{code}</p>
+                  <p>Para acompanhar as estatísticas, acesse { vars.HOST_APP + code }/stats</p>
                 </>
               )
             )}
--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -10,6 +10,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { StatsContainer, StatsRow, StatsBox, StatsBoxTitle } from './styles';
 
+import vars from '../../configs/vars';
 
 class StatsPage extends React.Component {
     constructor(props) {
@@ -58,7 +59,7 @@ class StatsPage extends React.Component {
                     </StatsContainer>
                 ) : (
                     <StatsContainer className="text-center">
-                        <p><b>https://pitu.tk/{shortenedURL.code}</b></p>
+                        <p><b>{ vars.HOST_APP + shortenedURL.code }</b></p>
                         <p>Redireciona para:<br/>{shortenedURL.url}</p>
                         <StatsRow>
                             <StatsBox>
